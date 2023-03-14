24f6eff753ae8f339ad2a44e5a22afcff755aa49
Imran Ansari <90140301+MuhammadImranAnsari@users.noreply.github.com>
2023-03-15 00:06:56 +0530

    Add files via upload

---
+++ quiz.js
@@ -0,0 +1,102 @@
+let quiz = [
+    {
+        question: "HTML stands for ?",
+        option: [
+            "HighText Machine Language",
+            "HyperText Markup Language",
+            "HyperText and links Markup Language",
+            "None of these",
+        ],
+        answer: 2,
+    },
+    {
+        question: "What does CSS stand for?",
+        option: [
+            "Colorful Style Sheets",
+            "Computer Style Sheets",
+            "Creative Style Sheets",
+            "Cascading Style Sheets",
+        ],
+        answer: 4,
+    },
+    {
+        question: "The correct sequence of HTML tags for starting a webpage is ?",
+        option: [
+            "HTML, Body, Title, Head",
+            "Head, Title, HTML, body",
+            "HTML, Head, Title, Body",
+            "Head, Title, HTML, body",
+        ],
+        answer: 3,
+    },
+    {
+        question: "Which of the following element is responsible for making the text bold in HTML?",
+        option: [
+            "<b>",
+            "<pre>",
+            "<a>",
+            "<br>",
+        ],
+        answer: 1,   
+    },
+    {
+        question: "Which of the following tag is used to embed css in html page?",
+        option: [
+            "<!DOCTYPE html>",
+            "<style>",
+            "<css>",
+            "<script>",
+        ],
+        answer: 2,   
+    },
+    {
+        question: "Which of the following CSS framework is used to create a responsive design?",
+        option: [
+            "bootstrap",
+            "rails",
+            "django",
+            "larawell",
+        ],
+        answer: 1,   
+    },
+    {
+        question: "How to create an ordered list (a list with the list items in numbers) in HTML?",
+        option: [
+            "<pre>",
+            "<b>",
+            "<ul>",
+            "<ol>",
+        ],
+        answer: 4,   
+    },
+    {
+        question: " In HTML, we use the <hr> tag for __",
+        option: [
+            "horizontal ruler",
+            "new line",
+            "new paragraph",
+            "vertical ruler",
+        ],
+        answer: 1,   
+    },
+    {
+        question: "Which type of JavaScript language is __",
+        option: [
+            "Object-Oriented",
+            "Assembly-language",
+            "Object-Based",
+            "High-level",
+        ],
+        answer: 3,   
+    },
+    {
+        question: " Which one is the HTML document’s root tag?",
+        option: [
+            "<title>",
+            "<body>",
+            "<head>",
+            "<html>",
+        ],
+        answer: 4,   
+    },
+]
